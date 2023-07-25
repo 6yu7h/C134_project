e89763b8b89d0436457bde58d54c498d93e61d79
6yu7h <114726087+6yu7h@users.noreply.github.com>
2023-07-25 10:14:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,4 +5,25 @@ function setup()
 	
 	canvas = createCanvas(500, 450);
 	canvas.position(560, 150);
+	
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log('PoseNet is Intialized');
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+		{
+			console.log(results);
+		}
+}
+
+function draw()
+{
+	background('#969A97');
 }
